d7a7366aa1fd53f02dd068211bf7d3d893cbad54
AeronKarl <aeron.toledo@gmail.com>
2022-08-19 11:11:59 +0800

    User login

--- js/userLogin.js
+++ js/userLogin.js
@@ -4,19 +4,35 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-auth.js'
 import { getFirestore, collection, onSnapshot, query, where, setDoc, doc, deleteDoc, updateDoc, getDoc } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js'
 
-
-
 //Firebase Config
 import { firebaseConfig } from './firebase.js';
 
 //Initialization
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const auth = getAuth();
+const db = getFirestore();
 
-const userLogin = document.querySelector('#loginButton');
-console.log(userLogin)
-userLogin.addEventListener("click", (e) => {
-    console.log("hello")
-    alert("hello");
-})
+const userLog = document.querySelector('#userLogin');
+userLog.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    //getting info from form
+    const email = userLog['UserEmail'].value;   
+    const pass = userLog['UserPassword'].value;
+
+    console.log(email, pass);
+
+    signInWithEmailAndPassword(auth, email, pass)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    location.assign("AdminManageTeacher.html");
+    
+  })
+  .catch((error) => {
+    const errorMessage = error.message;
+    alert(errorMessage);
+    
+  });   
+
+});
